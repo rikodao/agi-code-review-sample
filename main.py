acc568d303c5273534b1360ac07a17ada4a9110b
rikodao <rikodao@gmail.com>
2023-10-08 02:45:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,65 +3,32 @@ import botocore
 import urllib
 import json
 
-
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    prehook(event, context)
-    bedrock_runtime = boto3.client('bedrock-runtime')
-    s3 = boto3.client('s3')
-
+    prehookForDebug(event, context)
     
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     
-    
+    text = getCode(bucket, key)
 
-    # S3からオブジェクトを取得
-    obj = s3.get_object(Bucket=bucket, Key=key)
-    
-    # オブジェクトのボディ(中身)を文字列として取得
-    text = obj['Body'].read().decode('utf-8') 
-    
-    print(text)
-
-
-    modelId = 'anthropic.claude-v2'
-    # modelId = 'anthropic.claude-instant-v1'
-    accept = 'application/json'
-    contentType = 'application/json'
-
-    def prompt(text):
-        return '\n\nHuman:以下のLambda上で動くPythonで書かれたプログラムのコードを、[変数名の適切さ]、[リファクタリングの余地]、[バグの有無]の観点で、レビューしてください。\n' + text + '\n\nAssistant:'
-    
-    body = json.dumps({
-        "prompt": prompt(text),
-        "max_tokens_to_sample": 8191,
-        "temperature": 0.1,
-        "top_p": 0.9,
-    })
-
-    # APIレスポンスからBODYを取り出す
-    response = bedrock_runtime.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
-    response_body = json.loads(response.get('body').read())
-    # text
-    print(response_body)
-    
-    completion = response_body.get("completion")
-    sendSNSTopicMessage(completion)
+    completion = codeReviewWithBedrock(text)
+    sendSNSTopicMessage(key, completion)
     
     
     # return json.dumps(response_body, indent=2)
 
-def prehook(event, context):
+def prehookForDebug(event, context):
     print('botocore vertion: {0}'.format(botocore.__version__))
     print('boto3 vertion: {0}'.format(boto3.__version__))
     print("Received event: " + json.dumps(event, indent=2))
 
-def sendSNSTopicMessage(message):
+def sendSNSTopicMessage(title, message):
     # sns = boto3.client('sns')
     # topicArn = '000000000000000000000000000000000000000:lambda-review'
     message= {
-            'Title': title,
+            'Title': title + "のレビューをしまっせ",
             'Message': message
             }
     # sns.publish(TopicArn=topicArn, Subject=title, Message=message)
@@ -74,3 +41,35 @@ def sendSNSTopicMessage(message):
       Message=message,
     )
     
+def codeReviewWithBedrock(code):
+    bedrock_runtime = boto3.client('bedrock-runtime')
+    modelId = 'anthropic.claude-v2'
+    accept = 'application/json'
+    contentType = 'application/json'
+
+    def prompt(text):
+        return '\n\nHuman:以下のLambda上で動くPythonで書かれたプログラムのコードを、[変数名の適切さ]、[リファクタリングの余地]、[バグの有無]の観点で、10年来の友達として関西弁で正直にレビューしてな。\n' + text + '\n\nAssistant:'
+    
+    body = json.dumps({
+        "prompt": prompt(code),
+        "max_tokens_to_sample": 8191,
+        "temperature": 0.1,
+        "top_p": 0.9,
+    })
+
+    # APIレスポンスからBODYを取り出す
+    response = bedrock_runtime.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+    response_body = json.loads(response.get('body').read())
+    # text
+    print(response_body)
+    
+    completion = response_body.get("completion")
+    return completion
+
+
+def getCode(bucket, key):
+    obj = s3.get_object(Bucket=bucket, Key=key)
+    
+    # オブジェクトのボディ(中身)を文字列として取得
+    text = obj['Body'].read().decode('utf-8') 
+    return text 
