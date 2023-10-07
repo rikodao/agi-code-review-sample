3a32e6dd41f53090db0df080b1d3e1c285373a7c
rikodao <rikodao@gmail.com>
2023-10-08 01:31:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import botocore
 import urllib
 import json
 
-bedrock_runtime = boto3.client('bedrock-runtime')
-# bedrock_runtime = boto3.client(service_name='bedrock-runtime')
 
 
 def lambda_handler(event, context):
     prehook(event, context)
+    bedrock_runtime = boto3.client('bedrock-runtime')
+    s3 = boto3.client('s3')
+
+    
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     
-    s3 = boto3.client('s3')
+    
 
     # S3からオブジェクトを取得
     obj = s3.get_object(Bucket=bucket, Key=key)
