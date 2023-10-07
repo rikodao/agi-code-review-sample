d2af9da9171edbcac9db1fc7bcae43093b772f8f
rikodao <rikodao@gmail.com>
2023-10-08 01:29:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import boto3
 import botocore
 import urllib
 import json
-print('botocore vertion: {0}'.format(botocore.__version__))
-print('boto3 vertion: {0}'.format(boto3.__version__))
 
 bedrock_runtime = boto3.client('bedrock-runtime')
 # bedrock_runtime = boto3.client(service_name='bedrock-runtime')
-# test12
+
 
 def lambda_handler(event, context):
     print('botocore vertion: {0}'.format(botocore.__version__))
@@ -25,9 +23,7 @@ def lambda_handler(event, context):
     text = obj['Body'].read().decode('utf-8') 
     
     print(text)
-    
-    # プロンプトに設定する内容を取得
-    # text = event.get('prompt')
+
 
     modelId = 'anthropic.claude-v2'
     # modelId = 'anthropic.claude-instant-v1'
@@ -35,7 +31,7 @@ def lambda_handler(event, context):
     contentType = 'application/json'
 
     def prompt(text):
-        return '\n\nHuman:以下のプログラムのコードをレビューしてください。\n' + text + '\n\nAssistant:'
+        return '\n\nHuman:以下のプログラムのコードを、[変数名の適切さ]、[リファクタリングの余地]、[バグの有無]の観点で、レビューしてください。\n' + text + '\n\nAssistant:'
     
     body = json.dumps({
         "prompt": prompt(text),
@@ -49,3 +45,5 @@ def lambda_handler(event, context):
     response_body = json.loads(response.get('body').read())
     # text
     print(response_body)
+    completion = response_body.get("completion")
+    return completion
