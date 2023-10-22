06aa317a0ea8216928e50fc82767116a1a4da163
EC2 Default User <ec2-user@ip-172-31-20-108.ec2.internal>
2023-10-22 13:09:20 +0000

    handson

--- src/main.py
+++ src/main.py
@@ -71,3 +71,32 @@ def sendSNSTopicMessage(message):
       TopicArn=topic_arn,    
       Message=json.dumps(Message)
      )
+     
+
+def get_all_keys(bucket, prefix, keys, marker, recursive):
+    s3 = boto3.client('s3')
+    if recursive:
+        response = s3.list_objects(
+            Bucket=bucket, Prefix=prefix, Marker=marker)
+    else:
+        response = s3.list_objects(
+            Bucket=bucket, Prefix=prefix, Marker=marker, Delimiter='/')
+
+    # keyがNoneのときは初期化
+    if keys is None:
+        keys = []
+
+    if 'CommonPrefixes' in response:
+        keys.extend([content['Prefix']
+                    for content in response['CommonPrefixes']])
+    if 'Contents' in response:  
+        keys.extend([content['Key'] for content in response['Contents']])
+        if 'IsTruncated' in response:
+            return get_all_keys(bucket=bucket, prefix=prefix, keys=keys, marker=keys[-1], recursive=recursive)
+    return keys
+    
+def ls(bucket, prefix, recursive):
+    paths = []
+    paths = get_all_keys(
+        bucket, prefix, recursive=recursive)
+    return paths
