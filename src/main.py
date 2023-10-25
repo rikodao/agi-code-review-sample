100f213587907bc8bef47358a9c51bf966da488b
EC2 Default User <ec2-user@ip-172-31-20-108.ec2.internal>
2023-10-25 14:12:01 +0000

    demo push

--- src/main.py
+++ src/main.py
@@ -71,7 +71,33 @@ def sendSNSTopicMessage(message):
       TopicArn=topic_arn,    
       Message=json.dumps(Message)
      )
-     
+
+# Function to Upload file to S3 Bucket
+def upload_file_to_s3(file_name, bucket, object_name=None):
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+# Function to list S3 Bucket
+def list_files_in_s3_bucket(bucket):
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.list_objects(Bucket=bucket)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return response['Contents']
+    
+
 def get_all_keys(bucket, prefix, keys, marker, recursive):
     s3 = boto3.client('s3')
     if recursive:
