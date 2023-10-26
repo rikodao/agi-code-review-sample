c758da7d875eb818e768f407c2595b3159b677ad
EC2 Default User <ec2-user@ip-172-31-20-108.ec2.internal>
2023-10-26 08:04:28 +0000

    demo push

--- src/main.py
+++ src/main.py
@@ -75,21 +75,6 @@ def sendSNSTopicMessage(message):
      )
 
 
-def upload_file_to_s3(file_name, bucket, object_name=None):
-    
-    if object_name is None:
-        object_name = file_name
-
-    
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return Trues
-
-
 def list_s3_objects(bucket):
     s3_client = boto3.client('s3')
     response = s3_client.list_objects_v2(Bucket=bucket)
